7f222be47feb3888c318f6d0e60e842abeb6ff22
stephanie <stephaniealor7@gmail>
2023-06-19 09:52:42 +0100

    commit-one

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ hamburger.addEventListener('click', () => {
 
 });
 
-document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
